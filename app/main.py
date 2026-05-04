d1c348d85329746558ea4518d99ba1cfa054d1ea
jacKlinc <jack@inne.io>
2026-05-04 10:30:32 -0600

    Passed token in header and comments

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
-import json
-from datetime import datetime
-
-from fastapi import FastAPI, status, HTTPException, Response
+from fastapi import FastAPI, status, HTTPException, Header
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
@@ -13,11 +10,10 @@ from app.api.routes.auth import encrypt_payload, decrypt_payload
 
 app = FastAPI()
 
-# TODO status codes
-
 
 @app.post("/auth/register")
 def register(username: str, email: str, password: str):
+    """Create user in database"""
     hashed = hash_password(password)
     new_user = Users(username=username, email=email, hashed_password=hashed)
     with Session(engine) as session:
@@ -28,20 +24,24 @@ def register(username: str, email: str, password: str):
 
 @app.post("/auth/token")
 def get_token(username: str, password: str):
+    """Login to get valid token to make requests
+
+    Raises:
+        HTTPException: 404: User not found
+        HTTPException: 401: User found, password failed
+    """
     stmt = select(Users.hashed_password).where(Users.username == username)
-    response = {}
-    # Look up user by username and verify password
     with Session(engine) as session:
         result = session.execute(stmt).scalars().first()
 
         if not result:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
             )
 
         if verify_password(password, result):
+            response = {"status_code": 200}
             response["token"] = encrypt_payload({"user_id": username, "role": "admin"})
-            response["status_code"] = 200
             return response
 
     raise HTTPException(
@@ -50,8 +50,8 @@ def get_token(username: str, password: str):
 
 
 @app.get("/routes/{route_id}")
-def get_route(token: str, route_id: int):
-    # TODO pass token in header
+def get_route(route_id: int, token: str = Header(None)):
+    """Returns specific route ID"""
     decrypt_payload(token)
     stmt = select(Routes).where(Routes.id == route_id)
     with Session(engine) as session:
