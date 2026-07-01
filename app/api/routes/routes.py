a042a4d3c0990b274147f525182e35285ced80fc
jacKlinc <jack@inne.io>
2026-06-30 20:06:18 -0700

    Redis cache-aside pattern working locally

--- app/api/routes/routes.py
+++ app/api/routes/routes.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from pygeohash import encode
+import redis
 
 from app.core.security import decrypt_payload
 from app.db.session import get_db, get_async_db
@@ -26,6 +27,7 @@ limiter = Limiter(
 # Authenticated requests usually get a higher limit than anonymous
 # They are easier to track so are given 10-100x rate limit
 router = APIRouter(prefix="/routes")
+redis_client = redis.Redis("redis", port=6379, decode_responses=True)
 
 
 @limiter.limit("5/minute", per_method=True)
@@ -38,6 +40,9 @@ async def get_route(
 ):
     """Returns specific route ID"""
     decrypt_payload(token)
+    cached = redis_client.json().get(str(route_id))
+    if cached:
+        return cached
     stmt = select(Routes).where(Routes.id == route_id)
     result = await session.execute(stmt)
     route = result.scalar()
@@ -47,6 +52,13 @@ async def get_route(
             status_code=status.HTTP_404_NOT_FOUND, detail="Route not found"
         )
     logger.info("Fetched route id=%s", route_id)
+    route_dict = {
+        c.name: getattr(route, c.name).isoformat()
+        if isinstance(getattr(route, c.name), datetime)
+        else getattr(route, c.name)
+        for c in route.__table__.columns
+    }
+    redis_client.json().set(str(route_id), "$", route_dict)
     return route
 
 
@@ -105,7 +117,7 @@ async def create_route(
         lat=route.lat,
         lon=route.lon,
         geohash=encode(route.lat, route.lon),
-        created_at=datetime.now(), # asyncpg does not allow timezone-aware datetimes while sync one does
+        created_at=datetime.now(),  # asyncpg does not allow timezone-aware datetimes while sync one does
     )
     session.add(r)
     await session.commit()
--- redis-hw/main.py
+++ redis-hw/main.py
@@ -3,6 +3,21 @@ import redis
 redis_client = redis.Redis("localhost", port=6379, decode_responses=True)
 
 redis_client.set("my-key", "wow")
+redis_client.geoadd("bikes:rentable", [-122.27652, 37.805186, "station:1"])
+redis_client.geoadd("bikes:rentable", [-122.274, 37.82, "station:2"])
 
 key = redis_client.get("my-key")
 print(key)
+
+# https://redis.io/docs/latest/develop/data-types/geospatial/
+# https://youtu.be/qftiVQraxmI
+res4 = redis_client.geosearch(
+    "bikes:rentable",
+    longitude=-122.27652,
+    latitude=37.805186,
+    radius=5,
+    unit="km",
+    sort="DESC",
+    count=1,
+)
+print(res4)
